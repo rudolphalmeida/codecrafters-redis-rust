f0570732d9a7400c3e8a02f2630fadd8c67b5549
Rudolph Almeida <rudolf1.almeida@gmail.com>
2024-04-06 14:16:27 +1100

    Handle concurrent clients

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,37 @@
-use std::{
-    io::{Read, Write},
+use std::{error::Error, io::ErrorKind};
+
+use tokio::{
+    io::AsyncWriteExt,
     net::{TcpListener, TcpStream},
 };
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => handle_connection(stream),
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            let _ = handle_connection(socket).await;
+        });
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let mut buf = [0x00; 512];
-    while let Ok(_) = stream.read(&mut buf) {
-        let _ = stream.write_all("+PONG\r\n".as_bytes());
+async fn handle_connection(mut socket: TcpStream) -> Result<(), Box<dyn Error>> {
+    loop {
+        socket.readable().await?;
+
+        let mut buf = [0; 512];
+        match socket.try_read(&mut buf) {
+            Ok(0) => break,
+            Ok(_) => {
+                socket.writable().await?;
+                socket.write("+PONG\r\n".as_bytes()).await?;
+            }
+            Err(ref e) if e.kind() == ErrorKind::WouldBlock => continue,
+            Err(e) => return Err(e.into()),
+        }
     }
+
+    Ok(())
 }
