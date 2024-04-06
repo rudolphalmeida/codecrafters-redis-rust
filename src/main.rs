184eaf2aba2109a37a81ac195c3381a3c1298299
Rudolph Almeida <rudolf1.almeida@gmail.com>
2024-04-06 11:18:31 +1100

    Respong to PING

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
-use std::net::TcpListener;
+use std::{
+    io::Write,
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
+            Ok(stream) => handle_connection(stream),
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let _ = stream.write_all("+PONG\r\n".as_bytes());
 }
